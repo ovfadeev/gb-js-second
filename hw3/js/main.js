f698ff09ac84dab8b6cf6211bea1a2d3e55d74fc
o.fadeev <o.fadeev>
2018-01-12 11:20:57 +0300

    template

--- hw3/js/main.js
+++ hw3/js/main.js
@@ -1,6 +1,15 @@
+/* variables */
+var idFormName = 'js-form-name';
+var idFormEmail = 'js-form-email';
+var idFormPhone = 'js-form-phone';
+var idFormMessage = 'js-form-message';
+var idFormSubmit = 'js-form-submit';
+
+/* variables str */
 var str1 = '';
 var str2 = '';
 
+/* functions */
 
 window.onload = function () {
 
