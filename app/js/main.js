fefd251ebd7649132cdf9201a0afddad18cd2d16
o.fadeev <o.fadeev@live.ru>
2018-01-27 19:05:30 +0300

    hwEnd

--- app/js/main.js
+++ app/js/main.js
@@ -159,6 +159,83 @@ var moduleApp = {
       }
     });
   },
+  'validationForm':function($submitBtn,submitFunction){
+    $submitBtn = $submitBtn || $('.js-form-submit');
+    submitFunction = submitFunction || false;
+    $submitBtn.closest('form').addClass('is-form-validation');
+    $submitBtn.click(function(e){
+      var $this = $(this);
+      if ($this.hasClass('disabled')) { return false; }
+      var $form  = $this.closest('form');
+      var $forms = $form.find('[data-validate]');
+      var result = formChecking($forms,true);
+      if (result) {
+        if (submitFunction) {
+          $this.addClass('disabled');
+          submitFunction();
+        } else {
+          return true;
+        }
+      } else {
+        $forms.on('keyup keypress change', function() {
+          var $current = $(this);
+          setTimeout(function(){ formChecking($current); }, 50);
+        });
+      }
+      e.preventDefault();
+    });
+
+    $(document).on('keydown',function(e){
+      if(e.keyCode == 13) {
+        var $thisFocus = $(document.activeElement);
+        if ($thisFocus.is('textarea')) { return true; alert('123'); }
+        if ($thisFocus.closest('.form-select').length) { return true; }
+        if ($thisFocus.closest('.is-form-validation').length) { $submitBtn.trigger('click'); }
+      }
+    });
+
+    function formChecking($inp,onFocus) {
+      onFocus = onFocus || false;
+      var noError = true;
+      $inp.each(function(ind,elm){
+        var $this = $(elm);
+        var mask = $this.data('validate');
+        var value = $this.val();
+        var placeHolder = $this.attr('placeholder');
+        if (mask == 'text') {
+          if ((value.length < 1) || (value == placeHolder)) {
+            noError = false;
+            $this.closest('.input').addClass('show-error');
+            if (onFocus) { $this.focus(); onFocus = false; }
+          } else { $this.closest('.input').removeClass('show-error'); }
+        }
+        if (mask == 'textarea') {
+          if ((value.length < 1) || (value == placeHolder)) {
+            noError = false;
+            $this.closest('.textarea').addClass('show-error');
+            if (onFocus) { $this.focus(); onFocus = false; }
+          } else { $this.closest('.textarea').removeClass('show-error'); }
+        }
+        if (mask == 'email') {
+          var regex = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+          if (!regex.test(value) || (value == placeHolder)) {
+            noError = false;
+            $this.closest('.input').addClass('show-error');
+            if (onFocus) { $this.focus(); onFocus = false; }
+          } else { $this.closest('.input').removeClass('show-error'); }
+        }
+        if (mask == 'phone') {
+          var regex = /^\+7\(([0-9]{3})+\)([0-9]{3})+\-([0-9]{4})$/;
+          if (!regex.test(value) || (value == placeHolder)) {
+            noError = false;
+            $this.closest('.input').addClass('show-error');
+            if (onFocus) { $this.focus(); onFocus = false; }
+          } else { $this.closest('.input').removeClass('show-error'); }
+        }
+      });
+      return noError;
+    }
+  },
 }
 
 $(document).ready(function(){
@@ -183,5 +260,9 @@ $(document).ready(function(){
 
   /* --- check cart --- */
   moduleApp.checkBasket();
+  /* --- end --- */
+
+  /* --- validation form --- */
+  moduleApp.validationForm();
   /* --- end --- */
 });
